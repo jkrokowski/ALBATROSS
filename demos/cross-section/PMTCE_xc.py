688ce6d3b7125947cffc49dfb3ec5c3489f19f4d
jkrokowski <jkrokowski@ucsd.edu>
2023-04-28 09:12:46 -0700

    add stiffness matrix, get disp coeff

--- demos/cross-section/PMTCE_xc.py
+++ demos/cross-section/PMTCE_xc.py
@@ -2,20 +2,22 @@
 # ================
 
 from mpi4py import MPI
+import dolfinx.cpp.mesh
 from dolfinx import mesh,plot
-from dolfinx.fem import Constant,FunctionSpace,Function,form,assemble_scalar,VectorFunctionSpace,Expression,TensorFunctionSpace
+from dolfinx.fem import Constant,FunctionSpace,Function,form,assemble_scalar,VectorFunctionSpace,Expression,TensorFunctionSpace,locate_dofs_geometrical
 from dolfinx.fem.petsc import create_vector,assemble_matrix,assemble_vector
-from ufl import (FiniteElement,split,MixedElement,dot,lhs,rhs,Identity,inner,outer,TrialFunction,TestFunction,Measure,grad,exp,sin,SpatialCoordinate,FacetNormal,indices,as_tensor,as_matrix,as_vector,VectorElement,TensorElement,Dx)
+from ufl import (sym,FiniteElement,split,MixedElement,dot,lhs,rhs,Identity,inner,outer,TrialFunction,TestFunction,Measure,grad,exp,sin,SpatialCoordinate,FacetNormal,indices,as_tensor,as_matrix,as_vector,VectorElement,TensorElement,Dx)
 from petsc4py import PETSc
 import pyvista
 import numpy as np
 from scipy.linalg import null_space
+import matplotlib.pylab as plt
 
 # Create mesh and define function space
-N = 2
+N = 1
 domain = mesh.create_unit_square(MPI.COMM_WORLD,N,N, mesh.CellType.quadrilateral)
-# domain = mesh.create_unit_square(MPI.COMM_WORLD,N,N, mesh.CellType.triangle)
 Ue = VectorElement("CG",domain.ufl_cell(),1,dim=3)
+#TODO:check on whether TensorFxnSpace is more suitable for this
 V = FunctionSpace(domain, MixedElement([Ue,Ue,Ue,Ue]))
 
 #displacement and test functions
@@ -43,74 +45,74 @@ n = FacetNormal(domain)
 #material parameters
 E = 100 #70e9
 nu = 0.2
-
-# _lam = (E*nu)/((1+nu)*(1-2*nu))
-# mu = E/(2*(1+nu))
+_lam = (E*nu)/((1+nu)*(1-2*nu))
+mu = E/(2*(1+nu))
 
 #d x d indentity matrix
 delta = Identity(d)
 
 # C_ufl = (E/(1+nu))*(0.5*(delta_ik*delta_jl + delta_jk*delta_il) + (nu/(1-2*nu))*delta_ij*delta_kl)
-#TODO: rewrite with lame parameters for clarity
-C = as_tensor((E/(1+nu))*(0.5*(delta[i,k]*delta[j,l] \
-                + delta[j,k]*delta[i,l]) \
-                    + (nu/(1-2*nu))*delta[i,j]*delta[k,l])  ,(i,j,k,l))
+# C = as_tensor((E/(1+nu))*(0.5*(delta[i,k]*delta[j,l] \
+#                 + delta[j,k]*delta[i,l]) \
+#                     + (nu/(1-(2*nu)))*delta[i,j]*delta[k,l])  ,(i,j,k,l))
+
+C = as_tensor(_lam*(delta[i,j]*delta[k,l]) \
+                + mu*(delta[i,k]*delta[j,l]+ delta[i,l]*delta[j,k])  ,(i,j,k,l))
 
 #sub-tensors of stiffness tensor
 Ci1k1 = as_tensor(C[i,0,k,0],(i,k))
-Ci1kB = as_tensor([[[C[i, 0, k, l] for l in range(1, 3)]
+Ci1kB = as_tensor([[[C[i, 0, k, l] for l in [1,2]]
               for k in range(d)] for i in range(d)])
 Ciak1 = as_tensor([[[C[i, j, k, 0] for k in range(d)]
-              for j in range(1,3)] for i in range(d)])
-CiakB = as_tensor([[[[C[i, j, k, l] for l in range(1,3)]
-              for k in range(d)] for j in range(1,3)] 
+              for j in [1,2]] for i in range(d)])
+CiakB = as_tensor([[[[C[i, j, k, l] for l in [1,2]]
+              for k in range(d)] for j in [1,2]] 
               for i in range(d)])
-
+     
 #partial derivatives of displacement:
-ubar_B = as_tensor([Dx(ubar,B) for B in range(2)]).T
-uhat_B = as_tensor([Dx(uhat,B) for B in range(2)]).T
-utilde_B = as_tensor([Dx(utilde,B) for B in range(2)]).T
-ubreve_B = as_tensor([Dx(ubreve,B) for B in range(2)]).T
-
+ubar_B = grad(ubar)
+uhat_B = grad(uhat)
+utilde_B = grad(utilde)
+ubreve_B = grad(ubreve)
 
 #partial derivatives of shape fxn:
-vbar_a = as_tensor([Dx(vbar,a) for a in range(2)]).T
-vhat_a = as_tensor([Dx(vhat,a) for a in range(2)]).T
-vtilde_a = as_tensor([Dx(vtilde,a) for a in range(2)]).T
-vbreve_a = as_tensor([Dx(vbreve,a) for a in range(2)]).T
+vbar_a = grad(vbar)
+vhat_a = grad(vhat)
+vtilde_a = grad(vtilde)
+vbreve_a = grad(vbreve)
 
 #traction free boundary conditions
-Tbar = as_tensor(Ciak1[i,a,k]*uhat[k]*n[a],(i)) \
-     + as_tensor(CiakB[i,a,k,B]*ubar_B[k,B]*n[a],(i))
-That = 2*as_tensor(Ciak1[i,a,k]*utilde[k]*n[a],(i)) \
-     + as_tensor(CiakB[i,a,k,B]*uhat_B[k,B]*n[a],(i))
-Ttilde = 3*as_tensor(Ciak1[i,a,k]*ubreve[k]*n[a],(i)) \
-     + as_tensor(CiakB[i,a,k,B]*utilde_B[k,B]*n[a],(i))
-Tbreve = as_tensor(CiakB[i,a,k,B]*ubreve_B[k,B]*n[a],(i))
+Tbar = Ciak1[i,a,k]*uhat[k]*n[a]*vbar[i]*ds \
+          + CiakB[i,a,k,B]*ubar_B[k,B]*n[a]*vbar[i]*ds 
+That = 2*Ciak1[i,a,k]*utilde[k]*n[a]*vhat[i]*ds \
+          + CiakB[i,a,k,B]*uhat_B[k,B]*n[a]*vhat[i]*ds
+Ttilde = 3*Ciak1[i,a,k]*ubreve[k]*n[a]*vtilde[i]*ds \
+          + CiakB[i,a,k,B]*utilde_B[k,B]*n[a]*vtilde[i]*ds 
+Tbreve = CiakB[i,a,k,B]*ubreve_B[k,B]*n[a]*vbreve[i]*ds 
 
 # equation 1,2,3
-L1 = inner(2*Ci1k1,(outer(vtilde,utilde)))*dx \
-     + inner(Ci1kB,(outer(vhat,uhat_B)))*dx \
-     - inner(Ciak1,(outer(vhat_a,uhat)))*dx \
-     - inner(CiakB,outer(vbar_a,ubar_B))*dx \
-     + inner(Tbar,vbar)*ds
+L1= 2*Ci1k1[i,k]*utilde[k]*vbar[i]*dx\
+     + Ci1kB[i,k,B]*uhat_B[k,B]*vbar[i]*dx \
+     - Ciak1[i,a,k]*uhat[k]*vbar_a[i,a]*dx \
+     - CiakB[i,a,k,B]*ubar_B[k,B]*vbar_a[i,a]*dx \
+     # + Tbar
 
-# equation 4,5,6
-L2 = inner(6*Ci1k1,(outer(vbreve,ubreve)))*dx\
-     + inner(2*Ci1kB,(outer(vtilde,utilde_B)))*dx \
-     - inner(2*Ciak1,(outer(vtilde_a,utilde)))*dx \
-     - inner(CiakB,outer(vhat_a,uhat_B,))*dx\
-     + inner(That,vhat)*ds
+# # equation 4,5,6
+L2 = 6*Ci1k1[i,k]*ubreve[k]*vhat[i]*dx\
+     + 2*Ci1kB[i,k,B]*utilde_B[k,B]*vhat[i]*dx \
+     - 2*Ciak1[i,a,k]*utilde[k]*vhat_a[i,a]*dx \
+     - CiakB[i,a,k,B]*uhat_B[k,B]*vhat_a[i,a]*dx \
+     # + That
 
 # equation 7,8,9
-L3 = inner(3*Ci1kB,(outer(vbreve,ubreve_B)))*dx \
-     - inner(3*Ciak1,(outer(vbreve_a,ubreve)))*dx \
-     - inner(CiakB,outer(vbreve_a,utilde_B))*dx\
-     + inner(Ttilde,vtilde)*ds
+L3 = 3*Ci1kB[i,k,B]*ubreve_B[k,B]*vtilde[i]*dx \
+     - 3*Ciak1[i,a,k]*ubreve[k]*vtilde_a[i,a]*dx \
+     - CiakB[i,a,k,B]*utilde_B[k,B]*vtilde_a[i,a]*dx\
+     # + Ttilde
 
 #equation 10,11,12
-L4= -inner(CiakB,outer(vbreve_a,ubreve_B))*dx\
-     + inner(Tbreve,vbreve)*ds
+L4= -CiakB[i,a,k,B]*ubreve_B[k,B]*vbreve_a[i,a]*dx\
+     # + Tbreve
 
 Residual = L1+L2+L3+L4
 
@@ -119,7 +121,7 @@ LHS = lhs(Residual)
 RHS = Constant(domain,0.0)*v[1]*dx
 
 #assemble system matrices
-A = assemble_matrix(form(LHS))
+A = assemble_matrix(form(Residual))
 A.assemble()
 b=create_vector(form(RHS))
 with b.localForm() as b_loc:
@@ -128,122 +130,156 @@ assemble_vector(b,form(RHS))
 
 print(A.getSize())
 print(b.getSize())
-m,n=A.getSize()
+m,n1=A.getSize()
 
-Anp = A.getValues(range(m),range(n))
+Anp = A.getValues(range(m),range(n1))
 print("rank:")
 print(np.linalg.matrix_rank(Anp))
 
 nullspace = null_space(Anp)
 print(nullspace.shape)
 
-print("stiffness matrix:")
-print(Anp)
+# print("stiffness matrix:")
+# print(Anp)
 Usvd,sv,Vsvd = np.linalg.svd(Anp)
 # nullspace = A.getNullSpace()
 # print(nullspace.test(A))
 
-print(sv[-13:-1])
+sols = Vsvd[:,-12:]
+Anp_diag = np.diag(Anp)
 
+# plt.spy(Anp)
+# plt.show()
+
+#add in dirichlet BC to prevent rigid body modes?
+
+# use right singular vectors (nullspace) to find corresponding
+# coefficient solutions given arbitrary coefficient vector c
+c = np.vstack([np.zeros((6,1)),np.ones((6,1))])
+u_coeff=sols@c
+def get_vtx_to_dofs(domain,V):
+     '''
+     input: subspace to find DOFs in
+     output: map of DOFs related to their corresponding vertices
+     '''
+     V0, V0_to_V = V.collapse()
+     dof_layout = V0.dofmap.dof_layout
+
+     num_vertices = domain.topology.index_map(0).size_local + domain.topology.index_map(0).num_ghosts
+     vertex_to_par_dof_map = np.zeros(num_vertices, dtype=np.int32)
+     num_cells = domain.topology.index_map(
+          domain.topology.dim).size_local + domain.topology.index_map(
+          domain.topology.dim).num_ghosts
+     c_to_v = domain.topology.connectivity(domain.topology.dim, 0)
+     for cell in range(num_cells):
+          vertices = c_to_v.links(cell)
+          dofs = V0.dofmap.cell_dofs(cell)
+          for i, vertex in enumerate(vertices):
+               vertex_to_par_dof_map[vertex] = dofs[dof_layout.entity_dofs(0, i)]
+
+     geometry_indices = dolfinx.cpp.mesh.entities_to_geometry(
+          domain, 0, np.arange(num_vertices, dtype=np.int32), False)
+     bs = V0.dofmap.bs
+     vtx_to_dof = np.zeros((num_vertices,bs), dtype=np.int32)
+     for vertex, geom_index in enumerate(geometry_indices):
+          par_dof = vertex_to_par_dof_map[vertex]
+          for b in range(bs):
+               vtx_to_dof[vertex, b] = V0_to_V[par_dof*bs+b]
+     # vtx_to_dof = np.reshape(vtx_to_dof, (-1,1))
+
+     return vtx_to_dof
+
+ubar_vtx_to_dof = get_vtx_to_dofs(domain,V.sub(0))
+uhat_vtx_to_dof = get_vtx_to_dofs(domain,V.sub(1))
+utilde_vtx_to_dof = get_vtx_to_dofs(domain,V.sub(2))
+ubreve_vtx_to_dof = get_vtx_to_dofs(domain,V.sub(3))
+
+# dofs = V.dofmap.list.array
+# ubar_dofs = V.sub(0).dofmap.list.array
+# uhat_dofs = V.sub(1).dofmap.list.array
+# utilde_dofs = V.sub(2).dofmap.list.array
+# ubreve_dofs = V.sub(3).dofmap.list.array
+
+# num_vertices = domain.topology.index_map(0).size_local + domain.topology.index_map(0).num_ghosts
+# vertex_to_dof_map = np.empty(num_vertices, dtype=np.int32)
+# num_cells = domain.topology.index_map(
+#     domain.topology.dim).size_local + domain.topology.index_map(
+#     domain.topology.dim).num_ghosts
+# c_to_v = domain.topology.connectivity(domain.topology.dim, 0)
+# for cell in range(num_cells):
+#     vertices = c_to_v.links(cell)
+#     dofs = V.sub(2).dofmap.cell_dofs(cell)
+#     for i, vertex in enumerate(vertices):
+#         vertex_to_dof_map[vertex] = dofs[V.sub(2).dofmap.dof_layout.entity_dofs(0, i)]
+
+#construct displacement fxn space
+U = FunctionSpace(domain,Ue)
+U.dofmap
+u_sol = Function(U)
+# print(domain.topology.connectivity(0,1))
+# print(V.dofmap.dofs())
+print()
+
+
+#get map from mixedspace to displacement
+# points = mesh.points
+
+# def u(ubar,uhat,utilde,ubreve,x1,x2,x3):
+#      '''
+#      x: location along beam axis
+#      ubar,uhat,utilde,ubreve: coeff. fxns
+#      '''
+#      return ubar(x2,x3)+uhat(x2,x3)*x1+utilde(x2,x3)*x1**2 + ubreve(x2,x3)*x1**3
+
+# map each node solution to u_sol given u_nod_coeff 
+
+# construct stress tensor based on u_sol
+
+
+#integrate stresses over cross-section and construct xc load vector
+
+#compute K1 from Load vector and arbitrary coefficient vector
+
+#compute K2 by solving matrix equation c^T*K2*c = L^t*K1^-T*K2*K1^-1*L
+
+
+print()
 exit()
-# Solution Function
-uh = Function(V)
+sigma = C[i,j,k,l]*grad(sols)
+# exit()
+# # Solution Function
+# uh = Function(V)
 
-# Create Krylov solver
-solver = PETSc.KSP().create(A.getComm())
-solver.setOperators(A)
+# # Create Krylov solver
+# solver = PETSc.KSP().create(A.getComm())
+# solver.setOperators(A)
 
-# Create vector that spans the null space
-nullspace = PETSc.NullSpace().create(constant=True,comm=MPI.COMM_WORLD)
-A.setNullSpace(nullspace)
+# # Create vector that spans the null space
+# nullspace = PETSc.NullSpace().create(constant=True,comm=MPI.COMM_WORLD)
+# A.setNullSpace(nullspace)
 
-# orthogonalize b with respect to the nullspace ensures that 
-# b does not contain any component in the nullspace
-nullspace.remove(b)
+# # orthogonalize b with respect to the nullspace ensures that 
+# # b does not contain any component in the nullspace
+# nullspace.remove(b)
 
-# Finally we are able to solve our linear system :
-solver.solve(b,uh.vector)
+# # Finally we are able to solve our linear system :
+# solver.solve(b,uh.vector)
 
 
-# #======= COMPUTE TORSIONAL CONSTANT =========#
-# # This requires computing the shear center and computing the warping 
-# # function based on the location of that shear center
-
-# #area
-# A = assemble_scalar(form(1.0*dx))
-# print(A)
-# #first moment of area / A (gives the centroid)
-# x_G = assemble_scalar(form(x[0]*dx)) / A
-# y_G = assemble_scalar(form(x[1]*dx)) / A
-# print(x_G)
-# print(y_G)
-# #second moment of area 
-# Ixx= assemble_scalar(form(((x[1]-y_G)**2)*dx))
-# Iyy = assemble_scalar(form(((x[0]-x_G)**2)*dx))
-# print(Ixx)
-# print(Iyy)
-
-# Ixy = assemble_scalar(form(((x[0]-x_G)*(x[1]-y_G))*dx))
-# Iwx = assemble_scalar(form(((x[1]-y_G)*uh)*dx))
-# Iwy = assemble_scalar(form(((x[0]-x_G)*uh)*dx))
-# print("product and warping")
-# print(Ixy)
-# print(Iwx)
-# print(Iwy)
-
-# xs = (Iyy*Iwy-Ixy*Iwx)/(Ixx*Iyy-Ixy**2)
-# ys = -(Ixx*Iwx-Ixy*Iwy)/(Ixx*Iyy-Ixy**2)
-# print("shear center:")
-# print(xs)
-# print(ys)
-
-# w1_expr = Expression(-ys*x[0]+xs*x[1],V.element.interpolation_points())
-# w1 = Function(V)
-# w1.interpolate(w1_expr)
-
-# w = Function(V)
-# w.x.array[:] = uh.x.array[:] + w1.x.array[:]
-
-# #compute derivatives of warping function
-# W = VectorFunctionSpace(domain, ("CG", 1))
-# grad_uh = grad(w)
-# grad_uh_expr = Expression(grad_uh, W.element.interpolation_points())
-# grad_u = Function(W)
-# grad_u.interpolate(grad_uh_expr)
-
-# #separate out partial derivatives
-# dudx_expr = Expression(grad_uh[0], V.element.interpolation_points())
-# dudx = Function(V)
-# dudx.interpolate(dudx_expr)
-
-# dudy_expr = Expression(grad_uh[1], V.element.interpolation_points())
-# dudy = Function(V)
-# dudy.interpolate(dudy_expr)
-
-# Kwx = assemble_scalar(form(((x[0]-x_G)*dudy)*dx))
-# Kwy = assemble_scalar(form(((x[1]-y_G)*dudx)*dx))
-
-# print(Kwx)
-# print(Kwy)
-
-# K = Ixx+Iyy+Kwx-Kwy
-# print("Torsional Constant:")
-# print(K)
-
-# #======= PLOT WARPING FUNCTION =========#
-# # tdim = domain.topology.dim
-# # topology, cell_types, geometry = plot.create_vtk_mesh(domain, tdim)
-# # grid = pyvista.UnstructuredGrid(topology, cell_types, geometry)
-# # plotter = pyvista.Plotter()
-# # plotter.add_mesh(grid, show_edges=True,opacity=0.25)
-# # u_topology, u_cell_types, u_geometry = plot.create_vtk_mesh(V)
-# # u_grid = pyvista.UnstructuredGrid(u_topology, u_cell_types, u_geometry)
-# # u_grid.point_data["u"] = uh.x.array.real
-# # u_grid.set_active_scalars("u")
-# # plotter.add_mesh(u_grid.warp_by_scalar("u",factor=2), show_edges=True)
-# # plotter.view_vector((-0.25,-1,0.5))
-# # if not pyvista.OFF_SCREEN:
-# #     plotter.show()
+#======= PLOT WARPING FUNCTION =========#
+tdim = domain.topology.dim
+topology, cell_types, geometry = plot.create_vtk_mesh(domain, tdim)
+grid = pyvista.UnstructuredGrid(topology, cell_types, geometry)
+plotter = pyvista.Plotter()
+plotter.add_mesh(grid, show_edges=True,opacity=0.25)
+# u_topology, u_cell_types, u_geometry = plot.create_vtk_mesh(V)
+# u_grid = pyvista.UnstructuredGrid(u_topology, u_cell_types, u_geometry)
+# u_grid.point_data["u"] = uh.x.array.real
+# u_grid.set_active_scalars("u")
+# plotter.add_mesh(u_grid.warp_by_scalar("u",factor=2), show_edges=True)
+# plotter.view_vector((-0.25,-1,0.5))
+# if not pyvista.OFF_SCREEN:
+#     plotter.show()
 
 # # #plot warping
 # tdim = domain.topology.dim
