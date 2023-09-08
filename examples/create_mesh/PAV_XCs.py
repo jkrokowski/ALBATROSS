5213eb8cf7dc6a58020fb33c1903b8876b74931e
jkrokowski <jkrokowski@ucsd.edu>
2023-09-08 16:40:31 -0700

    update gmsh script to extract oml curves at XC

--- examples/create_mesh/PAV_XCs.py
+++ examples/create_mesh/PAV_XCs.py
@@ -45,22 +45,65 @@ xmin,ymin,zmin,xmax,ymax,zmax = gmsh.model.get_bounding_box(-1,-1)
 dx = xmax-xmin
 dy = ymax-ymin
 dz = zmax-zmin
-print(dx,dy,dz)
+# print(dx,dy,dz)
 
-plane = gmsh.model.occ.addRectangle(xmin,ymin,zmin,dx,dz)
-print(plane)
+plane = gmsh.model.occ.addRectangle(xmin-dx,ymin,zmin-dz,3*dx,3*dz)
 gmsh.model.occ.rotate([(2,plane)],xmin,ymin,zmin,1,0,0,math.pi/2)
-gmsh.model.occ.translate([(2,plane)],0,.1,0)
+gmsh.model.occ.translate([(2,plane)],0,1,-.25)
+gmsh.model.occ.synchronize()
 
 def getRelevantTags(cutplane_tag,model_tags):
-    pts = np.zeros((6,len(model_tags)))
+    #fxn finds surfaces that have bounding boxes that contain cutplane y-coord
+    pts = np.zeros((len(model_tags),6))
     for i,tag in enumerate(model_tags):
-        pts[:,i] = gmsh.model.occ.getBoundingBox(2,tag)
-    print(gmsh.model.occ.get_bounding_box(2,cutplane_tag)[1])
-    print(pts[1,:])
-    print(pts[4,:])
+        pts[i,:] = gmsh.model.occ.getBoundingBox(2,tag)
+        print(pts[i,:])
+    _,planemin,_,_,planemax,_ = gmsh.model.occ.get_bounding_box(2,cutplane_tag)
+    print(planemin,planemax)
+    print(model_tags)
+    for i in range(pts.shape[0]):
+        print(i+1,pts[i,[1,4]])
+        print(pts[i,1] <= planemin,pts[i,4]>=planemax)
+    return [i+1 for i,pt in enumerate(pts[:,[1,4]]) if (pt[0]<=planemin and pt[1]>= planemax) ]
     
 relevant_tags = getRelevantTags(plane,surfs_tags)
+print(relevant_tags)
+object_tags = list(zip(np.ones_like(relevant_tags)*2,relevant_tags))
+# print(object_tags)
+t1 = time.time()
+intersections = []
+for object_tag in object_tags:
+    intersection = gmsh.model.occ.fragment([object_tag],[(2,plane)],removeObject=False,removeTool=False)
+    gmsh.model.occ.synchronize()
+    # print(intersection)
+    intersections.append(intersection[0])
+t2 = time.time()
+print(t2-t1)
+
+print(intersections)
+
+#isolate curve segments from fragmentations
+planebb = gmsh.model.occ.get_bounding_box(2,plane)
+(xmin,ymin,zmin,xmax,ymax,zmax) = planebb
+eps = np.abs(.001*np.average((dx,dy,dz)))
+print(eps)
+curves = gmsh.model.occ.getEntitiesInBoundingBox(xmin+eps,ymin-eps,zmin+eps,xmax-eps,ymax+eps,zmax-eps,1)
+print(curves)
+
+thick = 0.00127
+
+#need to be able to handle interior and exterior separately
+curve_offset0 = gmsh.model.occ.copy([curves[0]])
+gmsh.model.occ.translate(curve_offset0,0,0,-thick)
+
+curve_offset1 = gmsh.model.occ.copy([curves[1]])
+gmsh.model.occ.translate(curve_offset1,thick,0,0)
+
+curve_offset2 = gmsh.model.occ.copy([curves[2]])
+gmsh.model.occ.translate(curve_offset2,0,0,thick)
+
+curve_offset3 = gmsh.model.occ.copy([curves[3]])
+gmsh.model.occ.translate(curve_offset3,-thick,0,0)
 
 
 # # option 1 SLOW
