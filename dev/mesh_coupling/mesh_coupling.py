790461f025333b88c99898fbecc821a7bebaeda1
jkrokowski <jkrokowski@ucsd.edu>
2024-03-13 18:14:58 -0700

    add occ and mesh coupling starts

--- dev/mesh_coupling/mesh_coupling.py
+++ dev/mesh_coupling/mesh_coupling.py
@@ -6,10 +6,13 @@ import numpy as np
 #cross-section mesh definition
 N = 5 #number of quad elements per side
 W = 1 #square height  
-H = 1 #square depth
+H = .1 #square depth
 points = [[-W/2,-H/2],[W/2, H/2]] #bottom left and upper right point of square
+points2 = [[-H/2,0],[H/2, W]] #bottom left and upper right point of square
 
-domain = ALBATROSS.utils.create_rectangle(points,[N,N])
+
+domain = ALBATROSS.utils.create_rectangle(points,[10*N,N])
+domain2 = ALBATROSS.utils.create_rectangle(points2,[N,10*N])
 
 mats = {'Unobtainium':{ 'TYPE':'ISOTROPIC',
                         'MECH_PROPS':{'E':100.,'nu':.2} ,
@@ -18,35 +21,20 @@ mats = {'Unobtainium':{ 'TYPE':'ISOTROPIC',
 
 #initialize cross-seciton object
 squareXS = ALBATROSS.cross_section.CrossSection(domain,mats)
+squareXS2 = ALBATROSS.cross_section.CrossSection(domain2,mats)
 
 #show me what you got
 squareXS.plot_mesh()
+squareXS2.plot_mesh()
 
 #compute the stiffness matrix
 squareXS.getXSStiffnessMatrix()
 
 np.set_printoptions(precision=3)
 
-#output flexibility matrix
-print('Flexibility matrix:')
-print(squareXS.S)
-
 #output stiffness matrix
 print('Stiffness matrix:')
 print(squareXS.K)
-
-print("Analytical axial stiffness (EA):")
-E = mats['Unobtainium']['MECH_PROPS']['E']
-A = W*H
-print(E*A)
-print("Computed Axial Stiffness:")
-print(squareXS.K[0,0])
-
-print("Analytical Bending stiffness (EI):")
-I = (W*H**3)/12
-print(E*I)
-print("Computed bending stiffness:")
-print(squareXS.K[4,4])
 
 
 
