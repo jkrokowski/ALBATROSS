fix derivatives

ALBATROSS/cross_section.py
@@ -537,6 +537,8 @@ class CrossSection:
                      for idx2 in range(6)] 
                         for idx1 in range(6)])
         
+        self.A = assemble_scalar(fem.form(self.A_form))
+        
         #store K1^-1 for recovery and sensitivity computation
         self.K1inv = np.linalg.inv(self.K1)
 
@@ -989,41 +991,41 @@ class CrossSection:
 
 
     def compute_pKpx(self):       
-        self.pK1px_form = [[derivative(self.K1_form[idx1][idx2],self.x,self.dX)
-                            for idx2 in range(6)] 
-                                for idx1 in range(6)]
+        # self.pK1px_form = [[derivative(self.K1_form[idx1][idx2],self.x,self.dX)
+        #                     for idx2 in range(6)] 
+        #                         for idx1 in range(6)]
         self.pK2px_form = [[derivative(self.K2_form[idx1][idx2],self.x,self.dX)
                             for idx2 in range(6)] 
                                 for idx1 in range(6)]
                 
-        self.pK1px_lol = [[petsc.assemble_vector(form(self.pK1px_form[idx1][idx2]))
-                        for idx2 in range(6)] 
-                            for idx1 in range(6)]
+        # self.pK1px_lol = [[petsc.assemble_vector(form(self.pK1px_form[idx1][idx2]))
+        #                 for idx2 in range(6)] 
+        #                     for idx1 in range(6)]
         self.pK2px_lol = [[petsc.assemble_vector(form(self.pK2px_form[idx1][idx2]))
                 for idx2 in range(6)] 
                     for idx1 in range(6)]
 
-        self.pK1px_sparse = sparse_mat_from_loflofvec(self.pK1px_lol)
+        # self.pK1px_sparse = sparse_mat_from_loflofvec(self.pK1px_lol)
         self.pK2px_sparse = sparse_mat_from_loflofvec(self.pK2px_lol)
         
-        self.pK1px = self.pK1px_sparse.toarray().reshape((6,6,self.pK1px_sparse.shape[1]))
+        # self.pK1px = self.pK1px_sparse.toarray().reshape((6,6,self.pK1px_sparse.shape[1]))
         self.pK2px = self.pK2px_sparse.toarray().reshape((6,6,self.pK2px_sparse.shape[1]))
 
 
         # #boundary dofs ([:,:,self.boundary_dofs])
         # self.boundary_nodes = locate_entities_boundary(self.msh,0,lambda x: np.ones_like(x[0]))
         
-        #TODO: can simplify this by flattening the K matrix into a vector, then the derivative is a matrix, not a third order tensor
-        #compact einsums:
-        term1 = np.einsum("ijm,ik,kl->jlm", self.pK1px, self.K2inv, self.K1)
-        term2 = -np.einsum("ij,jk,klm,ln,np->ipm", self.K1.T,self.K2inv,self.pK2px,self.K2inv,self.K1)
-        term3 = np.einsum("ij,jk,lkm->ilm", self.K1.T, self.K2inv, self.pK1px)
+        # #TODO: can simplify this by flattening the K matrix into a vector, then the derivative is a matrix, not a third order tensor
+        # #compact einsums:
+        # term1 = np.einsum("ijm,ik,kl->jlm", self.pK1px, self.K2inv, self.K1)
+        # term2 = -np.einsum("ij,jk,klm,ln,np->ipm", self.K1.T,self.K2inv,self.pK2px,self.K2inv,self.K1)
+        # term3 = np.einsum("ij,jk,lkm->ilm", self.K1.T, self.K2inv, self.pK1px)
 
         #full sensitivities
-        self.pKpx_original = term1 + term2 + term3 
+        # self.pKpx_original = term1 + term2 + term3 
         
-        pApx = petsc.assemble_vector(form(derivative(self.A_form,self.x,self.dX)))
-        term1 = 2*self.A*np.einsum('ij,k->ijk',self.K2inv,pApx.array)
+        self.pApx = petsc.assemble_vector(form(derivative(self.A_form,self.x,self.dX)))
+        term1 = 2*self.A*np.einsum('ij,k->ijk',self.K2inv,self.pApx.array)
         term2 = -np.einsum("ij,jkl,km->iml", self.K2inv,self.pK2px,self.K2inv)
 
         self.pKpx = term1 + self.A**2 * term2
@@ -1071,6 +1073,9 @@ class CrossSection:
         # self.pKpw = np.zeros((36,self.warping_functions[0].x.array.shape[0]*6))
         self.pKpw = np.zeros((6,6,6,self.warping_functions[0].x.array.shape[0]))
         warping_len = self.warping_functions[0].x.array.shape[0]
+
+        # pApw = petsc.assemble_vector(form(derivative(self.A_form,self.w)))
+        # term1 = 2*self.A*np.einsum('ij,k->ijk',self.K2inv,pApw.array)
         for idx3 in range(6):
             self.pK1pw_sparse = sparse_mat_from_loflofvec(self.pK1pw_lol[idx3])
             self.pK2pw_sparse = sparse_mat_from_loflofvec(self.pK2pw_lol[idx3])
@@ -1083,19 +1088,18 @@ class CrossSection:
             
             #TODO: can simplify this
             #compact einsums:
-            term1 = np.einsum("ijm,ik,kl->jlm", self.pK1pw, self.K2inv, self.K1)
-            term2 = -np.einsum("ij,jk,klm,ln,np->ipm", self.K1.T,self.K2inv,self.pK2pw,self.K2inv,self.K1)
-            term3 = np.einsum("ij,jk,lkm->ilm", self.K1.T, self.K2inv, self.pK1pw)
+            # term1 = np.einsum("ijm,ik,kl->jlm", self.pK1pw, self.K2inv, self.K1)
+            # term2 = -np.einsum("ij,jk,klm,ln,np->ipm", self.K1.T,self.K2inv,self.pK2pw,self.K2inv,self.K1)
+            # term3 = np.einsum("ij,jk,lkm->ilm", self.K1.T, self.K2inv, self.pK1pw)
 
-            term1 = np.einsum("ijm,ik,kl->jlm", self.pK1pw, self.K2inv, self.K1)
-            term2 = -np.einsum("ij,jk,klm,ln,np->ipm", self.K1.T,self.K2inv,self.pK2pw,self.K2inv,self.K1)
-            term3 = np.einsum("ij,jk,lkm->ilm", self.K1.T, self.K2inv, self.pK1pw)
+            term2 = -np.einsum("ij,jkl,km->iml", self.K2inv,self.pK2pw,self.K2inv)
 
             #full sensitivities
             # start = warping_len*idx3
             # stop = warping_len*(idx3+1)
             # self.pKpw[:,start:stop] = (term1 + term2 + term3).T.reshape((warping_len,36)).T
-            self.pKpw[:,:,idx3,] = (term1 + term2 + term3)
+            # self.pKpw[:,:,idx3,] = (term1 + term2 + term3)
+            self.pKpw[:,:,idx3,] = self.A**2 * term2
               
         # #get map from vtx to dofs to restrict to boundary (this only works for CG1)
         # self.boundary_dof_to_vertex_map = np.tile(np.arange(self.msh.geometry.x.shape[0]),self.VX.value_size)
@@ -1147,16 +1151,19 @@ class CrossSection:
             # #boundary dofs ([:,:,self.boundary_dofs])
             # self.boundary_nodes = locate_entities_boundary(self.msh,0,lambda x: np.ones_like(x[0]))
             
-            #TODO: can simplify this
-            #compact einsums:
-            term1 = np.einsum("ijm,ik,kl->jlm", self.pK1pl, self.K2inv, self.K1)
-            term2 = -np.einsum("ij,jk,klm,ln,np->ipm", self.K1.T,self.K2inv,self.pK2pl,self.K2inv,self.K1)
-            term3 = np.einsum("ij,jk,lkm->ilm", self.K1.T, self.K2inv, self.pK1pl)
+            # #TODO: can simplify this
+            # #compact einsums:
+            # term1 = np.einsum("ijm,ik,kl->jlm", self.pK1pl, self.K2inv, self.K1)
+            # term2 = -np.einsum("ij,jk,klm,ln,np->ipm", self.K1.T,self.K2inv,self.pK2pl,self.K2inv,self.K1)
+            # term3 = np.einsum("ij,jk,lkm->ilm", self.K1.T, self.K2inv, self.pK1pl)
+            
+            term2 = -np.einsum("ij,jkl,km->iml", self.K2inv,self.pK2pl,self.K2inv)
 
             #full sensitivities
             start = lm_len*idx3
             stop = lm_len*(idx3+1)
-            self.pKpl[:,start:stop] = (term1 + term2 + term3).reshape((36,lm_len))
+            # self.pKpl[:,start:stop] = (term1 + term2 + term3).reshape((36,lm_len))
+            self.pKpl[:,start:stop] = (self.A**2 * term2).reshape((36,lm_len))
                
         # #get map from vtx to dofs to restrict to boundary (this only works for CG1)
         # self.boundary_dof_to_vertex_map = np.tile(np.arange(self.msh.geometry.x.shape[0]),self.VX.value_size)

ALBATROSS/csdl_utils.py
@@ -225,8 +225,8 @@ class BeamMatrixFromWarping(csdl.CustomExplicitOperation):
         # assign method inputs to input dictionary
         self.declare_input('xy',inputs.xy)
         self.declare_input('xy_interior',inputs.xy_interior)
-        self.declare_input('w',inputs.w)
-        self.declare_input('lmbda',inputs.lmbda)
+        # self.declare_input('w',inputs.w)
+        # self.declare_input('lmbda',inputs.lmbda)
 
         # construct output of the model
         outputs = csdl.VariableGroup()
@@ -240,24 +240,18 @@ class BeamMatrixFromWarping(csdl.CustomExplicitOperation):
         #update boundary nodes:
         if self.boundary_nodes is not None: 
             self.xs.msh.geometry.x[self.boundary_nodes,0:2]=inputs['xy']
-        
+
         #update interior nodes
         if self.interior_nodes is not None: 
             self.xs.msh.geometry.x[self.interior_nodes,0:2]=inputs['xy_interior']
         else: 
             self.xs.msh.geometry.x[:,0:2]=inputs['xy']
 
-        
-        # w_len = self.xs.warping_functions[0].x.array.shape[0]
-        # lmbda_len = self.xs.lmbdas[0].x.array.shape[0]
-        # offset_w = 0
-        # offset_l = 0
-        for i in range(6):
-            self.xs.warping_functions[i].x.array[:] = inputs['w'][:,i]
-            self.xs.lmbdas[i].x.array[:] = inputs['lmbda'][:,i]
-        # inputs['w'] = np.vstack([self.xs.warping_functions[i].x.array for i in range(6)]).T
-        # inputs['lmbda'] = np.vstack([self.xs.lmbdas[i].x.array for i in range(6)]).T
-    
+        # for i in range(6):
+        #     self.xs.warping_functions[i].x.array[:] = inputs['w'][:,i]
+        #     self.xs.lmbdas[i].x.array[:] = inputs['lmbda'][:,i]
+        print('mesh coords:')
+        print(self.xs.msh.geometry.x[:,0:2])
         self.xs._compute_xs_stiffness_matrix()
 
         outputs['K'] = self.xs.K
@@ -266,42 +260,39 @@ class BeamMatrixFromWarping(csdl.CustomExplicitOperation):
     def compute_derivatives(self, inputs, outputs, derivatives):
         print('compute beam matrix derivatives...')
         #update boundary nodes:
-        if self.boundary_nodes is not None: 
-            self.xs.msh.geometry.x[self.boundary_nodes,0:2]=inputs['xy']
+        # if self.boundary_nodes is not None: 
+        #     self.xs.msh.geometry.x[self.boundary_nodes,0:2]=inputs['xy']
         
-        #update interior nodes
-        if self.interior_nodes is not None: 
-            self.xs.msh.geometry.x[self.interior_nodes,0:2]=inputs['xy_interior']
-        else: 
-            self.xs.msh.geometry.x[:,0:2]=inputs['xy']
+        # #update interior nodes
+        # if self.interior_nodes is not None: 
+        #     self.xs.msh.geometry.x[self.interior_nodes,0:2]=inputs['xy_interior']
+        # else: 
+        #     self.xs.msh.geometry.x[:,0:2]=inputs['xy']
         
-        # w_len = self.xs.warping_functions[0].x.array.shape[0]
-        # lmbda_len = self.xs.lmbdas[0].x.array.shape[0]
-        # offset_w = 0
-        # offset_l = 0
-        for i in range(6):
-            self.xs.warping_functions[i].x.array[:] = inputs['w'][:,i]
-            self.xs.lmbdas[i].x.array[:] = inputs['lmbda'][:,i]
-        # inputs['w'] = np.vstack([self.xs.warping_functions[i].x.array for i in range(6)]).T
-        # inputs['lmbda'] = np.vstack([self.xs.lmbdas[i].x.array for i in range(6)]).T
-    
-        self.xs._compute_xs_stiffness_matrix()
+        # for i in range(6):
+        #     self.xs.warping_functions[i].x.array[:] = inputs['w'][:,i]
+        #     self.xs.lmbdas[i].x.array[:] = inputs['lmbda'][:,i]
+        
+        # self.xs._compute_xs_stiffness_matrix()
                 
         pKpx = self.xs.compute_pKpx()
-        pKpw = self.xs.compute_pKpw()
-        pKpl = self.xs.compute_pKpl()
+        # pKpw = self.xs.compute_pKpw()
+        # pKpl = self.xs.compute_pKpl()
 
         #declare derivatives
         print('input vals:')
         print(inputs['xy'])
+        print('mesh coords:')
+        print(self.xs.msh.geometry.x[:,0:2])
         derivatives['K', 'xy'] = pKpx[:,self.xs.dofs_boundary]
+        # derivatives['K', 'xy'] = pKpx[:,np.concatenate([self.xs.dofs_x_boundary,self.xs.dofs_y_boundary])]
         derivatives['K', 'xy_interior'] = pKpx[:,self.xs.dofs_interior]
         # derivatives['K', 'xy'] = np.hstack([pKpx[:,self.xs.dofs_x_boundary],
         #                                 pKpx[:,self.xs.dofs_y_boundary]]) #return (36 x num_boundary_nodes*2)
         # derivatives['K', 'xy_interior'] = np.hstack([pKpx[:,self.xs.dofs_x_interior],
         #                                 pKpx[:,self.xs.dofs_y_interior]]) #return (36 x num_interior_nodes*2)
-        derivatives['K', 'w'] = pKpw #return (36 x num_warping_function_dofs*6) but need to be ordered  
-        derivatives['K', 'lmbda'] = pKpl #return (36 x 30*6)
+        # derivatives['K', 'w'] = pKpw #return (36 x num_warping_function_dofs*6) but need to be ordered  
+        # derivatives['K', 'lmbda'] = pKpl #return (36 x 30*6)
 
 
 class EllipticSmoothing(csdl.CustomExplicitOperation):

examples/csdl/custom_explicit_op_K_from_w_test.py
@@ -45,7 +45,6 @@ inputs.xy_interior = csdl.Variable(value=xy_interior,shape=xy_interior.shape,nam
 # xy_interior = inputs.xy_interior
 
 
-
 # #CONSTRUCT A BOUNDARY B-SPLINE (with a closed, uniform knot vector)
 # num_parametric = 6
 # bspline_degree=3
@@ -140,11 +139,17 @@ K00.name = 'K00'
 # xy_node0 = inputs.xy[0,:]
 xy_node0.name = 'xy_node0'
 
-dKdx_check = sim.check_totals(outputs_sec.K,inputs.xy,step_size=0.000001,print_results=True)
+dKdx_check = sim.check_totals(outputs_sec.K,inputs.xy,step_size=1e-6,print_results=True)
+dKdx = dKdx_check[outputs_sec.K,inputs.xy]['value']
+dKdx_FD = dKdx_check[outputs_sec.K,inputs.xy]['fd_value']
 
-#TODO: currently have accurate derivatives, except it seems that the partials are a bit scrambled for 
+#check norms across K matrix entries (for all x)
 for i in range(36):
-    print(i,np.linalg.norm(dKdx_check[outputs_sec.K,inputs.xy]['value'][i]-dKdx_check[outputs_sec.K,inputs.xy]['fd_value'][i]))
+    print(i,np.linalg.norm(dKdx[i,:]-dKdx_FD[i,:]))
+
+#check norms across x
+for i in range(xs.boundary_nodes.shape[0]*2):
+    print(i,np.linalg.norm(dKdx[:,i]-dKdx_FD[:,i]))
 
 dK00dx = sim.compute_totals(K00,reduced_xy)
 dK00dx_FD = sim.compute_totals(K00,reduced_xy,use_finite_difference=True,finite_difference_step_size=0.001)